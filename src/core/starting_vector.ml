f915f348dfe5082a74e85a23fd77cee6cf210e7a
K. <117524908+lambda0xE@users.noreply.github.com>
2023-12-21 02:33:39 +0000

    preparing for key initialization

---
+++ src/core/starting_vector.ml
@@ -0,0 +1,51 @@
+module type FIELD = sig
+  type t
+  val zero : t
+  val one : t
+  val add : t -> t -> t
+  val mul : t -> t -> t
+  val sub : t -> t -> t
+  val inv : t -> t
+  val rand : unit -> t
+end
+
+module PrimeField : FIELD with type t = Z.t = struct
+  type t = Z.t
+  let prime = Z.(nextprime (one lsl 1024))
+  let zero = Z.zero
+  let one = Z.one
+  let add = Z.add
+  let mul = Z.mul
+  let sub = Z.sub
+  let inv x = Z.invert x prime
+  let rand () =
+    let bits = Z.numbits prime in
+    let rec loop () =
+      let r = Z.(random_bits bits mod prime) in
+      if Z.(r >= prime) then loop () else r
+    in
+    loop ()
+end
+
+module Vector (F : FIELD) = struct
+  type t = F.t list
+
+  let init n : t = List.init n (fun _ -> F.rand ())
+  let map2 f a b = List.map2 f a b
+  let reduce = List.fold_left F.add F.zero
+end
+
+module StartingVector = struct
+  module V = Vector(PrimeField)
+
+  let phi x = 
+    let open PrimeField in
+    let r = rand () in
+    PrimeField.mul (PrimeField.add (PrimeField.mul x x) r) (PrimeField.add x one)
+
+  let generate n : PrimeField.t list =
+    let a = V.init n in
+    let p = V.init n in
+    let phi_applied = V.map2 (fun ai pi -> phi (PrimeField.mul ai pi)) a p in
+    List.map V.reduce (List.map (fun x -> [x]) phi_applied)
+end
